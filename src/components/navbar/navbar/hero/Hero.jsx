22612fa164c0bdb173af82f1880e5b346e98ae41
ThatOneMessedUpKid <aniketrana03010@gmail.com>
2024-06-17 16:24:39 +0530

    removed the contact me and see my works in hero.jss

--- src/components/navbar/navbar/hero/Hero.jsx
+++ src/components/navbar/navbar/hero/Hero.jsx
@@ -58,12 +58,12 @@ const Hero = () => {
                 <motion.h1 variants={textVariants}>
                     UI/UX designer and Web Developer
                 </motion.h1>
-                <motion.div variants={textVariants}  className="buttons">
+                {/* <motion.div variants={textVariants}  className="buttons">
                     <motion.button variants={textVariants} >See the Latest Works</motion.button>
     
                     
                     <motion.button variants={textVariants}>Contact Me</motion.button>
-                </motion.div>
+                </motion.div> */}
                 <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
             </motion.div>
         </div>
